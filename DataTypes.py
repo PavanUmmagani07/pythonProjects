101f7b32b5723114f2471af06339b1b36b134477
Pavan Ummagani <pavankalyanU@outlook.com>
2023-08-06 16:43:34 +0530

    python projects

---
+++ DataTypes.py
@@ -0,0 +1,17 @@
+#Mukhyanga naalugu data types untay mowa
+#String (Example ki “Nihar” , “My Name is Nihar” , “Nen python nerchkunta”)
+#Simple terms la cheppalante Stream of Characters.
+#Capital Letters ( A – Z )
+#Small Letters ( a – z )
+# Digits ( 0 – 9 )
+#Special Characters (~ ! @ # $ % ^ . ?,)
+#Space
+#Integer (Example ki 1,2,-9,0)
+#Float (Any number with Decimal points, Example ki -3.90, 4.5)
+#Boolean (True, False)
+#Declare a Variable and Assign the value and convert it into integer and print the type of value and value?
+
+Value = input()
+Value = int(Value)
+print(Value)
+print(type(Value))
