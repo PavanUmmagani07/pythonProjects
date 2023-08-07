d75486f4f4746d475a6940537f9e53c4ae521cdf
Pavan Ummagani <pavankalyanU@outlook.com>
2023-08-07 11:04:38 +0530

    RELATIONALOPERATORS Exercise

---
+++ RelationalOperatorsExercise.py
@@ -0,0 +1,54 @@
+#Write a program that reads a number and check if the given number is greater than 70
+A = int(input())
+print(A>70)
+
+#Write a program that reads two numbers and checks if the first number is greater than second number
+A = int(input())
+B = int(input())
+print(A>B)
+
+#Write a program that reads a number and checks if the given number is negative or not
+A = int(input())
+print(A<0)
+
+
+#Write a program that reads two words and checks if the given two words are same
+#Sample Input
+#JAM
+#JaM
+#Expected Output
+#False
+
+A = input()
+B = input()
+print(A==B)
+
+
+#Write a program that reads two numbers and checks if the given numbers are not the same
+A = int(input())
+B = int(input())
+print(A!=B)
+
+
+#Write program that reads two numbers and checks if first numbers is greater than or equal to second number?
+#Sample INPUT
+#82
+#86
+#Expected Output
+#A >= B is False
+A = int(input())
+B = int(input())
+Result = A>=B
+print("A >= B is "+Result)
+
+
+#Write a program that reads two flot numbers and checks if the first number is lessthan the second number?
+#Sample INPUT
+#82.6
+#86.9
+#Expected Output
+#A >= B is True
+A = float(input())
+B = float(input())
+Result = A<=B
+print("A <= B is "+Result)
