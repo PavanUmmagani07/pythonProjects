c1b43ccc60f5278982a871e06eb22fade17c9717
Pavan Ummagani <pavankalyanU@outlook.com>
2023-08-07 17:02:24 +0530

    Logical Operators

---
+++ LogicalOperators.py
@@ -0,0 +1,37 @@
+#LOGICAL OPERATORS
+#The logical operators are used to perform logical operations on Boolean values.
+#Gives True or False as the result.
+#Following are the logical operators
+#and
+#or
+#not
+
+#LOGICAL AND OPERATOR
+#Returns TURE, If both the Booleans are TRUE or else it gives FALSE
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+#Examples
+print((2<3)and(3>1))
+print((2<3)and(3<1))
+
+#Logical OR Operator
+#Gives TRUE, if any one of the booleans are True or else it gives False
+print(False or False)
+print(True or False)
+
+#Example
+print((2<3) or (3>4))
+
+#LOGICAl NOT Operator
+#Gives the OPPOSITE value of the given boolean
+print(not(True))
+
+#Example
+print(not(2>4))
+
+#SUMMARY
+#Logical AND Operator gives True if all the booleans are true.
+#Logical OR Operator gives True if any of the booleans are true.
+#Logical NOT Operator gives the opposite value
