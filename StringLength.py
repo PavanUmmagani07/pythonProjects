036573aadc26525c72431b2cabf3b90ddd80b968
Pkalyan07 <Pkalyan07@users.noreply.github.com>
2023-08-29 07:17:38 +0530

    strings

--- StringLength.py
+++
@@ -1,4 +0,0 @@
-#Length of String
-#"len()" returns the number of characters in a given string.
-Username = input()
-print(len(Username))
--- rough.py
+++ rough.py
@@ -1,18 +1,38 @@
 N = int(input())
-term_num = '1'
-sum = 0
-for i in range(1,(N+1)):
-    term = term_num*i
-    sum = sum+int(term)
-print(sum)
+for i in range(1,N+1):
+    left_Spaces = (N-i)*" "
+    hollow_Spaces = (i-2)*"  "
+    if(i==1):
+        print(left_Spaces+"*")
+    else:
+        print(left_Spaces+"* "+hollow_Spaces+"*")
 
-#While Loop
+for j in range(N,0,-1):
+    left_Spaces = (N-j)*" "
+    hollow_Spaces = (j-2)*"  "
+    if(j==1):
+        print(left_Spaces+"*")
+    else:
+        print(left_Spaces+"* "+hollow_Spaces+"*")
+
+
+
 N = int(input())
-term_num = '1'
-i = 1
-sum = 0
-while(i<(N+1)):
-    term = term_num*i
-    sum = sum+int(term)
-    i+=1
-print(sum)
+for i in range(1,N+1):
+    left_Spaces = (N-i)*" "
+    hollow_Spaces = (i-2)*"  "
+    if(i == 1):
+        print(left_Spaces+"*")
+    else:
+        print(left_Spaces+"* "+hollow_Spaces+"*")
+
+for j in range(1,N):
+    left_Spaces = j*" "
+    hollow_Spaces = (N-(j+1))*"  "
+    if(j==N):
+        print(left_Spaces+"*")
+    else:
+        print(left_Spaces+"* "+hollow_Spaces+"*")
+
+
+
