fa9e649e6d9b9ea135e2e16a682b6dab5428a002
Pkalyan07 <Pkalyan07@users.noreply.github.com>
2023-09-27 10:13:13 +0530

    Lists

--- Lists.py
+++ Lists.py
@@ -133,3 +133,66 @@ print(id(a))
 #Example
 list_a =[5,"Six",8.2,]
 print(id(list_a))
+
+a = 'Hello'
+print(id(a))
+a+='World!'
+print(id(a))
+
+#Modifying Lists
+list_a = [1,2,3]
+print(id(list_a))
+
+list_a = [1,2,3]
+list_b = [1,2,3]
+print(id(list_a[0]))
+print(id(list_b[0]))
+print(id(list_a))
+print(id(list_b))
+
+list_a = [1,2,3]
+list_b = list_a
+print(id(list_a))
+print(id(list_b))
+
+#Modifying Lists
+list_a = [1,2,3,5]
+list_b=list_a
+list_b[3]= 4
+print('list a: '+str(list_a))
+print('list b: '+str(list_b))
+
+list_a = [1,2]
+list_b = list_a
+list_a = [6,7]
+print('list a :'+ str(list_a))
+print('list b :'+str(list_b))
+
+list_a = [1,2]
+list_b = list_a
+list_a = list_a+[6,7]
+print('list a :'+ str(list_a))
+print('list b :'+str(list_b))
+
+
+#Compound assignment operator updates the objects in place
+#Compound assignment operators updates the same object
+list_a = [1,2]
+list_b = list_a
+list_a+=[6,7]
+print('list a:' + str(list_a))
+print('list b:' + str(list_b))
+
+
+list_a = [1,2]
+list_b = [3,list_a]
+list_a[1] = 4
+print(list_a)
+print(list_b)
+
+
+a =2
+list_a = [1,a]
+print(list_a)
+a =3
+print(list_a)
